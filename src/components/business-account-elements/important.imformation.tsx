453d48aca0eed87e0519f56351169045ba42e25b
Your Name <you@example.com>
2024-02-14 03:12:03 +0700

    add payment

--- src/components/business-account-elements/important.imformation.tsx
+++ src/components/business-account-elements/important.imformation.tsx
@@ -11,7 +11,7 @@ export const ImportantInformation: React.FC<IImportantInformationProps> = (props
         <>
             <div className={"w-full"}>
                 <div className="flex flex-row w-full contet-center items-center">
-                    <div className={`w-[3rem] h-[8rem] bg-gradient-to-r from-white bg-${props.information.color}-500 rounded-r-[10rem] mr-4`}></div>
+                    <div className={`w-[3rem] h-[8rem] bg-gradient-to-r from-white bg-${props.information.color}-500 rounded-r-full mr-4`}></div>
                     <div className={`w-[1.5rem] h-[1.5rem] bg-${props.information.color}-500 rounded-full text-center ml-2 mr-4`}><span className="text-white">i</span></div>
                     <div className="w-full ml-1">
                         <p>{props.information.title}</p>
